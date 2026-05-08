a439750dfc7ccdaf25d8125582a22b290fd946c0
Yiman <yiman2008@mail.ru>
2026-05-08 12:44:02 +0300

    database 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ sourceSets {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,5 @@
 rootProject.name = "Samsung2026"
 include(":app")
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
